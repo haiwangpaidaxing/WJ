e9e3a542eae9fb7d471fbc1360c08613ee9a6c55
MS-WPVSCGAKVHYV\Administrator <15014141271@163.com>
2023-02-23 00:00:41 +0800

    新地图

--- Assets/Script/03Game/MapManager.cs
+++ Assets/Script/03Game/MapManager.cs
@@ -22,6 +22,7 @@ public class MapManager : MonoBehaviour
     }
     public void CreateRoom(CreateRoomData createRoomData)
     {
+
         Vector3Int startPos = createRoomData.startPos;
         startPos.x = -1;
         for (int y = 0; y < createRoomData.groundDepth; y++)
@@ -34,14 +35,15 @@ public class MapManager : MonoBehaviour
                 if (y == createRoomData.groundDepth - 1)//地表层
                 {
                     groundTilemap.SetTile(startPos, createRoomData.groundTile[0]);
+                    createRoomData.groundList.Add(startPos);
                 }
                 else //泥土层
                 {
                     groundTilemap.SetTile(startPos, createRoomData.soilTile[0]);
                 }
-               
+
             }
-            
+
         }//基础地面
         for (int y = createRoomData.groundDepth; y < createRoomData.size.y; y++)
         {
@@ -50,27 +52,71 @@ public class MapManager : MonoBehaviour
             for (int x = 0; x < createRoomData.size.x; x++)
             {
                 startPos.x += 1;
-                if (x==0||x== createRoomData.size.x-1)
+                if (y == createRoomData.size.y - 1)
                 {
-                    groundTilemap.SetTile(startPos, createRoomData.groundTile[0]);
+                    groundTilemap.SetTile(startPos, createRoomData.roofWallTile[0]);
+                    createRoomData.roofList.Add(startPos);
                 }
-                else if (y == createRoomData.size.y-1)//房顶
+                else if (x == 0)
                 {
-
+                    groundTilemap.SetTile(startPos, createRoomData.leftWallTile[0]);
+                    createRoomData.leftWallList.Add(startPos);
                 }
-                Debug.Log(startPos);
+                else if (x == createRoomData.size.x - 1)
+                {
+                    groundTilemap.SetTile(startPos, createRoomData.rightWallTile[0]);
+                    createRoomData.rightWallList.Add(startPos);
+                }
 
             }
         }//基础地面
+        switch (createRoomData.roomExit)
+        {
+            case RoomExit.LeftRight:
+                TileBase[] startWallTile = new TileBase[2] {
+                    createRoomData.wallLeftSatrtTile[0] ,createRoomData.wallRightSatrtTile[0]};
+                EmbellishWall(createRoomData.leftWallList, startWallTile, createRoomData.rightWallTile[0], 1);
+                TileBase[] startWallTile1 = new TileBase[2] {
+                createRoomData.wallRightSatrtTile[0]  ,  createRoomData.wallLeftSatrtTile[0] };
+                EmbellishWall(createRoomData.rightWallList, startWallTile1, createRoomData.leftWallTile[0], -1);
+                break;
+            case RoomExit.LeftRightUp:
+                break;
+            case RoomExit.LeftRightDown:
+                break;
+        }
     }
+
+    public void EmbellishWall(List<Vector3Int> wallList, TileBase[] wallStartPoint, TileBase wallTile, int dir)
+    {
+        int height = Random.Range(3, wallList.Count );
+        for (int i = 0; i < height; i++)
+        {
+            groundTilemap.SetTile(wallList[i], null);
+        }
+        Vector3Int start1 = wallList[height];
+        Vector3Int start2 = wallList[height];
+        start2.x += dir;
+        groundTilemap.SetTile(start1, wallStartPoint[0]);
+        groundTilemap.SetTile(start2, wallStartPoint[1]);
+        for (int i = height + 1; i < wallList.Count; i++)
+        {
+            Vector3Int pos = wallList[i];
+            pos.x += dir;
+            groundTilemap.SetTile(pos, wallTile);
+        }
+    }
+
 }
 [System.Serializable]
 public class CreateRoomData
 {
     public TileBase[] groundTile;
     public TileBase[] soilTile;
-    public TileBase[] leftWall;
-    public TileBase[] rightWall;
+    public TileBase[] leftWallTile;
+    public TileBase[] rightWallTile;
+    public TileBase[] roofWallTile;
+    public TileBase[] wallLeftSatrtTile, wallRightSatrtTile;
     [Header("房间起始点")]
     public Vector3Int startPos;
     [Header("房间地面深度")]
@@ -80,6 +126,11 @@ public class CreateRoomData
     [Header("房间大小")]
     public Vector2Int size;
     public RoomExit roomExit;
+
+    public List<Vector3Int> groundList = new List<Vector3Int>();
+    public List<Vector3Int> rightWallList = new List<Vector3Int>();
+    public List<Vector3Int> leftWallList = new List<Vector3Int>();
+    public List<Vector3Int> roofList = new List<Vector3Int>();
 }
 public enum RoomExit
 {
---
+++ Assets/Script/03Game/TestMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * Input.GetAxisRaw("Horizontal") *5*Time.deltaTime;
+        transform.position += Vector3.up * Input.GetAxisRaw("Vertical") * 5 *Time.deltaTime;
+    }
+}
